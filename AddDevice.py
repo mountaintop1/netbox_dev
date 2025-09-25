b569d82634d62ba0159f9729770bc2e5639be2b1
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-09-25 15:20:27 -0600

    Update AddDevice.py

--- AddDevice.py
+++ AddDevice.py
@@ -30,7 +30,7 @@ class AddDevices(Script):
     class Meta:
         name = "Add New Device To Site"
         description = "Provision a New switch to Site"
-
+        commit_default = False
         fieldsets = (
             ('Device Object', ('device_name', 'switch_model', 'mgmt_address', 'gateway_address', 'is_stack_switch')),
             ('Site Object', ('site', 'mgmt_vlan', 'blan_vlan', 'guest_vlan')),
