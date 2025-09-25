81c7ede6397fa1fc53a62fa875f467d4ca415787
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-09-25 10:46:30 -0600

    Update AddDevice.py

--- AddDevice.py
+++ AddDevice.py
@@ -1,8 +1,8 @@
 from extras.scripts import *
 from django.utils.text import slugify
 
-from dcim.choices import DeviceStatusChoices, SiteStatusChoices
-from dcim.models import Device, DeviceRole, DeviceType, Site, Platform, Interface
+from dcim.choices import DeviceStatusChoices
+from dcim.models import Device, DeviceRole, DeviceType, Site, Platform, Interface, Manufacturer
 from ipam.models import IPAddress, VLAN, VLANGroup
 
 
@@ -123,66 +123,24 @@ class AddDevices(Script):
     )
     def run(self, data, commit):
 
-        # Create the new site
-        site = Site(
-            name=data['site_name'],
-            slug=slugify(data['site_name']),
-            status=SiteStatusChoices.STATUS_PLANNED
-        )
-        site.save()
-        self.log_success(f"Created new site: {site}")
-
         # Create access switches
         switch_role = DeviceRole.objects.get(name='Access Switch')
-        for i in range(1, data['switch_count'] + 1):
-            switch = Device(
-                device_type=data['switch_model'],
-                name=f'{site.slug.upper()}-SW-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=switch_role
-            )
-            switch.save()
-            self.log_success(f"Created new switch: {switch}")
-
-        # Create routers
-        router_role = DeviceRole.objects.get(name='WAN Router')
-        for i in range(1, data['router_count'] + 1):
-            router = Device(
-                device_type=data['router_model'],
-                name=f'{site.slug.upper()}-RTR-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=router_role
-            )
-            router.save()
-            self.log_success(f"Created new router: {router}")
-
-        # Create APs
-        ap_role = DeviceRole.objects.get(name='Wireless AP')
-        for i in range(1, data['ap_count'] + 1):
-            ap = Device(
-                device_type=data['ap_model'],
-                name=f'{site.slug.upper()}-AP-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=ap_role
-            )
-            ap.save()
-            self.log_success(f"Created new AP: {router}")
-        
-        # Create Servers
-        server_role = DeviceRole.objects.get(name='vSphere')
-        for i in range(1, data['server_count'] + 1):
-            server = Device(
-                device_type=data['server_model'],
-                name=f'{site.slug.upper()}-VSP-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=server_role
-            )
-            server.save()
-            self.log_success(f"Created new server: {router}")
+        platform = Platform.objects.get(slug='ios')
+        dt = DeviceType.objects.get(model=switch_model)
+        mfr = dt.manufacturer
+        switch = Device(
+            device_type=data['switch_model'],
+            name=data['device_name'],
+            site=data['site'],
+            status=DeviceStatusChoices.STATUS_ACTIVE,
+            role=switch_role,
+            platform=platform,
+            config_template='master_temp_acc_v1',
+            cf_gateway=data['gateway_address'],
+            manufacturer=mfr,
+        )
+        switch.save()
+        self.log_success(f"Created new switch: {switch} from {data}")
 
         # Generate a CSV table of new devices
         output = [
