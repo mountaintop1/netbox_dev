0f9503d423357f539b3e62398347ee98389b1baa
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-09-26 22:40:10 -0600

    Replace uplink_choices with CHOICES in dropdowns

--- device_onboarding.py
+++ device_onboarding.py
@@ -400,7 +400,7 @@ class DeviceOnboardingVersioning(Script):
         required=False
     )
     uplink_1 = ChoiceVar(
-        choices=uplink_choices,
+        choices=CHOICES,
         description="Uplink Interface drop-down",
         label='Uplink Interface'
     )
@@ -410,7 +410,7 @@ class DeviceOnboardingVersioning(Script):
         default='remotehost=os-z07-41ra0043-01-sw-lef-a; port=xe-0/0/18',
     )
     uplink_2 = ChoiceVar(
-        choices=uplink_choices,
+        choices=CHOICES,
         description="Uplink Interface drop-down",
         label='Uplink Interface'
     )
