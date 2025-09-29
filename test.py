1f8467627c866db5471f8b940d20960e9af8bb5d
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-09-28 21:55:17 -0600

    Add dynamic choice for selecting NetBox Sites

--- test.py
+++ test.py
@@ -2,6 +2,11 @@ from netbox.scripts import Script, ChoiceVar
 from dcim.models import Site
 
 class MyCustomScript(Script):
+    class Meta:
+        name = "test 1"
+        description = "Test dynamic var"
+        commit_default = False
+    
     my_dynamic_choice = ChoiceVar(
         description="Select a Site:",
         choices=[],
@@ -11,9 +16,8 @@ class MyCustomScript(Script):
     def run(self, data, commit):
         site_choices = [(site.slug, site.name) for site in Site.objects.all()]
         self.my_dynamic_choice.choices = site_choices
-
-        # Now, the user will see a dropdown with all NetBox Sites
+        
         selected_site_slug = data['my_dynamic_choice']
         selected_site = Site.objects.get(slug=selected_site_slug)
-
+        
         self.log_info(f"Selected site: {selected_site.name}")
