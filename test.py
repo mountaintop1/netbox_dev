4056ff46a20b97fcd989ee63ccc6f75ffac8690f
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-09-28 22:25:07 -0600

    Refactor site_choices to use generator expression

--- test.py
+++ test.py
@@ -14,12 +14,11 @@ class MyCustomScript(Script):
         commit_default = False
 
     def run(self, data, commit):
-        self.log_info(f"Selected site: {data}")
-        site_choices = [(site.slug, site.name) for site in Site.objects.all()]
+        site_choices = ((site.slug, site.name) for site in Site.objects.all())
         self.log_info(f"Selected site: {site_choices}")
         
         data['my_dynamic_choice'] = site_choices
-        self.log_info(f"Selected site: {data['my_dynamic_choice']}")
+        self.log_info(f"Selected site: {data}")
         
         #selected_site_slug = data['my_dynamic_choice']
         #selected_site = Site.objects.get(slug=selected_site_slug)
