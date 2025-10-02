aee1876ec929c330a200117c10b6af81d07075ae
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-10-02 11:57:20 -0600

    Update test_choice.py

--- test_choice.py
+++ test_choice.py
@@ -1,2 +1,83 @@
 from extras.scripts import *
 from django.utils.text import slugify
+
+
+class DynamicChioce(ChoiceVar):
+  pass
+
+
+CHOICES = (
+    ('TenGigabitEthernet1/1/1', 'Te1/1/1'),
+    ('TenGigabitEthernet1/1/2', 'Te1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+    ('TwentyFiveGigabitEthernet1/1/1', 'Twe1/1/1'),
+    ('TwentyFiveGigabitEthernet1/1/2', 'Twe1/1/2'),
+    ('GigabitEthernet1/1', 'Gi1/1'),
+    ('GigabitEthernet1/2', 'Gi1/2'),
+)
+
+LAG_CHOICES = (
+    ('Po1', 'Po1'),
+    ('Po2', 'Po2'),
+    ('Po3', 'Po3'),
+)
+
+choices1 = (
+    ('TenGigabitEthernet1/1/1', 'Te1/1/1'),
+    ('TenGigabitEthernet1/1/2', 'Te1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+)
+
+choices2 = (
+    ('GigabitEthernet1/1/1', 'Gi1/1/1'),
+    ('GigabitEthernet1/1/2', 'Gi1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+)
+
+choices3 = (
+    ('TwentyFiveGigabitEthernet1/1/1', 'Twe1/1/1'),
+    ('TwentyFiveGigabitEthernet1/1/2', 'Twe1/1/2'),
+    ('TwentyFiveGigabitEthernet1/1/3', 'Twe1/1/3'),
+    ('TwentyFiveGigabitEthernet1/1/4', 'Twe1/1/4'),
+)
+
+choices4 = (
+    ('GigabitEthernet1/1', 'Gi1/1'),
+    ('GigabitEthernet1/2', 'Gi1/2'),
+    ('GigabitEthernet1/3', 'Gi1/3'),
+    ('GigabitEthernet1/4', 'Gi1/4'),
+)
+
+CHOICES_BY_MODEL = {
+    "cisco-c9300l-24p-4x": choices1,
+    "cisco-c9300l-48uxg-4x": choices1,
+    "cisco-c9300lm-24u-4y": choices3,
+    "cisco-c9200cx-12p-2x2g": choices2,
+    "cisco-ie-4000-8gt8gp4g-e": choices4,
+}
+
+
+    
+class DeviceOnboarding(Script):
+
+    class Meta:
+        name = "Dynamic Choice Fields"
+        description = "Testing Dynamic chioce f"
+        commit_default = False
+
+    uplink_1 = ChoiceVar(
+        choices=CHOICES,
+        description="Uplink Interface drop-down",
+        label='Uplink Interface',
+    )
+    uplink_desc_a = StringVar(
+        description="Uplink Port 1 Interface Description",
+        label='Uplink Interface Description',
+        default='remotehost=os-z07-41ra0043-01-sw-lef-a; port=xe-0/0/18',
+    )
+
+    def run(self, data, commit):
+      pass
