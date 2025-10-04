dd217760b41368a0aebb2b9621abc5ff3f2022c7
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2025-10-04 17:27:19 -0600

    Update test_device.py

--- test_device.py
+++ test_device.py
@@ -46,7 +46,7 @@ class DeviceOnboarding(Script):
         model=Interface,
         label="Uplink Switch A Interface",
         query_params={
-            "cable_id__isnull": False,
+            "cable__id__isnull": True,
             "device_id": "$uplink_sw_a",
         }
     )
